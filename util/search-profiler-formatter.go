3240293655365e76bbe97890c16bdec8973a5b62
mluedtke <moritz.luedtke@bonprix.net>
2021-07-14 08:36:01 +0200

    - Refactoring

--- util/search-profiler-formatter.go
+++ util/search-profiler-formatter.go
@@ -5,8 +5,12 @@ import (
 )
 
 const (
-	tabSize    = 2
-	whitespace = " "
+	tabSize         = 2
+	whitespace      = " "
+	OpenBracket     = "("
+	OpenBracketPlus = "+("
+	ClosingBracket  = ")"
+	LineBreak       = "\n"
 )
 
 func FormatSearchProfilerOutput(searchProfilerOutput string) string {
@@ -17,7 +21,7 @@ func FormatSearchProfilerOutput(searchProfilerOutput string) string {
 	output = strings.Replace(output, "| ", "|\n", -1)
 	output = strings.Replace(output, " +", "\n+", -1)
 
-	outputAsArray := strings.Split(output, "\n")
+	outputAsArray := strings.Split(output, LineBreak)
 
 	currentIndentationLevel := 0
 	var indentedOutputArray []string
@@ -25,20 +29,20 @@ func FormatSearchProfilerOutput(searchProfilerOutput string) string {
 	for _, line := range outputAsArray {
 		line = strings.Trim(line, whitespace)
 
-		if strings.Contains(line, ")") {
+		if strings.Contains(line, ClosingBracket) {
 			currentIndentationLevel--
 		}
 
 		indentedOutputArray = append(indentedOutputArray, addIndentationToLine(line, currentIndentationLevel))
 
-		if strings.Contains(line, "(") {
+		if strings.Contains(line, OpenBracket) {
 			currentIndentationLevel++
-		} else if strings.Contains(line, "+(") {
+		} else if strings.Contains(line, OpenBracketPlus) {
 			currentIndentationLevel++
 		}
 	}
 
-	return strings.Join(indentedOutputArray, "\n")
+	return strings.Join(indentedOutputArray, LineBreak)
 }
 
 func addIndentationToLine(line string, indentationLevel int) string {
